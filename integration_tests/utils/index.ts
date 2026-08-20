6637027b65c3dd4b8c29c892279c77bdd1d41cbf
Ben Shimmin <bas@bas.me.uk>
2026-08-20 15:31:56 +0100

    Add stub offender utility function
    The integration tests need a bunch of tweaks so that the appointment
    tests know about the offender service. The stub is a bit verbose so
    let's wrap it in a utility function for easy invocation.

--- integration_tests/utils/index.ts
+++ integration_tests/utils/index.ts
@@ -1,4 +1,7 @@
-import { AppointmentSummaryDto } from '../../server/@types/shared'
+import { AppointmentDto, AppointmentSummaryDto } from '../../server/@types/shared'
+import caseDetailsSummaryFactory from '../../server/testutils/factories/caseDetailsSummaryFactory'
+import offenderFullFactory from '../../server/testutils/factories/offenderFullFactory'
+import unpaidWorkDetailsFactory from '../../server/testutils/factories/unpaidWorkDetailsFactory'
 import DateTimeFormats from '../../server/utils/dateTimeUtils'
 
 export default class Utils {
@@ -6,5 +9,19 @@ export default class Utils {
     const dateA = DateTimeFormats.isoToMilliseconds(a.date)
     const dateB = DateTimeFormats.isoToMilliseconds(b.date)
     return dateA - dateB
+  }
+
+  static stubOffenderFromAppointment(appointment: AppointmentDto) {
+    const { crn } = appointment.offender
+    const offender = offenderFullFactory.build({ crn })
+    const upwDetails = unpaidWorkDetailsFactory.build()
+    const caseDetailsSummary = caseDetailsSummaryFactory.build({
+      offender,
+      unpaidWorkDetails: [upwDetails, unpaidWorkDetailsFactory.build()],
+    })
+
+    cy.task('stubGetOffenderSummary', {
+      caseDetailsSummary,
+    })
   }
 }
